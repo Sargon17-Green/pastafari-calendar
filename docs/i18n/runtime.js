67b7d8269067381a4c67ba655a44d98f4752593d
bwtbdyqtmsprytgydym-cpu <bwtbdyqtmsprytgydym@gmail.com>
2026-08-13 19:01:37 +0300

    Update runtime.js

--- docs/i18n/runtime.js
+++ docs/i18n/runtime.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { LOCALES, getLocale, resolveLocale, translate } from "./registry.js";
+import { LOCALES, getLocale, resolveLocale, translate } from "./registry.js?v=7-search-compare";
 
 export const LANGUAGE_STORAGE_KEY = "pastafari.language";
 
