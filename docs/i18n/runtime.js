49173839cf92c39baa46a603573fef18cc25dad3
Sargon-17-Green <eliezer.green003@gmail.com>
2026-08-14 09:02:52 +0300

    Update runtime.js

--- docs/i18n/runtime.js
+++ docs/i18n/runtime.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { LOCALES, getLocale, resolveLocale, translate } from "./registry.js?v=7-search-compare";
+import { LOCALES, getLocale, resolveLocale, translate } from "./registry.js?v=8-year-structure";
 
 export const LANGUAGE_STORAGE_KEY = "pastafari.language";
 
