2404a870bd2195256cf64c4f3eadf1dc1a980b7c
Sargon-17-Green <eliezer.green003@gmail.com>
2026-08-18 07:56:37 +0300

    Add files via upload
    დამატებულია უკუ-თარიღის ძიების სრული ინტერფეისი და constraint solver-ის ინტეგრაცია GitHub Pages-ში. დამატებულია ჩვეულებრივი და გაფართოებული ძიება, SAME_AS_TARGET, ციკლური დამოკიდებულებების მხარდაჭერა, PWA/offline რესურსები, 89 ენის i18n, ახალი ტესტები და განახლებული SHA-256 manifest-ები. Forward გამოთვლის ალგორითმი არ შეცვლილა.

--- docs/i18n/runtime.js
+++ docs/i18n/runtime.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { LOCALES, getLocale, resolveLocale, translate } from "./registry.js?v=8-year-structure";
+import { LOCALES, getLocale, resolveLocale, translate } from "./registry.js?v=13-reverse-i18n";
 
 export const LANGUAGE_STORAGE_KEY = "pastafari.language";
 
