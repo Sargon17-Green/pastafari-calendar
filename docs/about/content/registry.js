ffb64f7fee337b1375d5c3189da17b4fadfc265b
Sargon 17 Green <bwtbdyqtmsprytgydym@gmail.com>
2026-09-25 18:55:32 +0300

    i18n(about): register all supported article locales

--- docs/about/content/registry.js
+++ docs/about/content/registry.js
@@ -1,14 +1,21 @@
 "use strict";
 
-export const ARTICLE_FALLBACK_LOCALE = "he";
+import { LOCALES } from "../../i18n/registry.js?v=20-about-i18n";
 
-export const ARTICLE_LOCALES = Object.freeze({
-  he: Object.freeze({
-    code: "he",
-    dir: "rtl",
-    asset: "./content/he.html?v=2-about-review",
-  }),
-});
+export const ARTICLE_FALLBACK_LOCALE = "he";
+export const ARTICLE_ASSET_REVISION = "3-about-i18n";
+
+export const ARTICLE_LOCALES = Object.freeze(Object.fromEntries(
+  LOCALES.map(({ code, dir, intlLocale }) => [
+    code,
+    Object.freeze({
+      code,
+      lang: intlLocale,
+      dir,
+      asset: `./content/${code}.html?v=${ARTICLE_ASSET_REVISION}`,
+    }),
+  ]),
+));
 
 export function resolveArticleLocale(code) {
   return ARTICLE_LOCALES[code] ?? ARTICLE_LOCALES[ARTICLE_FALLBACK_LOCALE];
